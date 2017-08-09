394f2553bde64561885456831315921b3dd0c0cf
Bill Burgess <wmhburgess@gmail.com>
2017-08-09 14:29:23 +1200

    adds empty "top-bar" component

---
+++ components/top-bar.js
@@ -0,0 +1,15 @@
+const React = require('react')
+const { connect } = require('react-redux')
+
+class TopBar extends React.Component {
+
+  render () {
+    return (
+      <div>this is the topbar</div>
+    )
+  }
+}
+
+
+
+module.exports = TopBar
