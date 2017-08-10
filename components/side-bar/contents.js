2ca268fee308125f714d224b5bfd88a8870d2323
Bill Burgess <wmhburgess@gmail.com>
2017-08-10 15:38:51 +1200

    adds functionality to search bar

--- components/side-bar/contents.js
+++ components/side-bar/contents.js
@@ -5,10 +5,31 @@ class Contents extends React.Component {
   render () {
 
     const dispatch = this.props.dispatch
-    const state = this.props.state || {contentsMenu: []}
-    const { contentsMenu } = state
+    const state = this.props.state || {contentsMenu: [{header: '', subheaders: []}], searchInput: ''}
+    const { contentsMenu, searchInput } = state
 
-    const contentsHeaders = contentsMenu.map(formatDisplay)
+    const shownSections = contentsMenu.filter(searchSection).map(searchSection)
+    const contentsHeaders = shownSections.map(formatDisplay)
+
+    function searchSection(section){
+      const subheaderMatches = section.subheaders.filter(filterBySearch)
+      if (filterBySearch(section.header)) {
+        return section
+      }
+      else if (section.subheaders.filter(filterBySearch).length) {
+        const filteredSubheaders = section.subheaders.filter(filterBySearch)
+        return Object.assign({}, section, {
+          subheaders: filteredSubheaders
+        })
+      }
+      else{
+        return false
+      }
+    }
+
+    function filterBySearch(string){
+      return string.toUpperCase().indexOf(searchInput.toUpperCase()) !== -1
+    }
 
     function formatDisplay (section) {
       let display = [(
