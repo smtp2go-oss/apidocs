279a8f21717b0038ba05f6abd192981b68b480f7
Bill Burgess <wmhburgess@gmail.com>
2017-08-18 11:53:18 +1200

    tidies code up

--- components/main-page/index.js
+++ components/main-page/index.js
@@ -1,48 +1,9 @@
 const React = require('react')
-import renderHTML from 'react-render-html'
+const renderHTML= require('react-render-html')
 
 const { getHtml } = require('../../services/get-html')
 
-const Readme = require('./readme')
-const Statistics = require('./statistics')
-const Emails = require('./emails')
-const EmailBlocklist = require('./email-blocklist')
-const SmtpUsers = require('./smtp-users')
-
 const intitialState = require('../../state')
-
-function setDisplay (page, state) {
-  switch (page) {
-    case '/readme':
-      return <Readme />
-
-      break;
-
-    case '/statistics':
-      return <Statistics />
-
-      break;
-
-    case '/emails':
-      return <Emails />
-
-      break;
-
-    case '/emailBlocklist':
-      return <EmailBlocklist />
-
-      break;
-
-    case '/smtpUsers':
-      return <SmtpUsers />
-
-      break;
-
-    default:
-      return
-
-  }
-}
 
 class Mainpage extends React.Component {
 
@@ -64,11 +25,10 @@ class Mainpage extends React.Component {
     const { page, htmlDisplay } = state
     const html = htmlDisplay
       ? renderHTML(htmlDisplay)
-      : 'beep boop'
-    console.log(htmlDisplay)
+      : null
 
     return (
-      <div className='smtp2go-doc-container'>
+      <div className='docContainer'>
         {html}
       </div>
     )
